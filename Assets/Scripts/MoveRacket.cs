5f33aff0536c3ae3dda7cf36e3451548f012d67f
Michel Kassis <michel.kassis120@gmail.com>
2017-11-10 02:29:21 -0500

    Initial commit

---
+++ Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour {
+
+	public float speed = 30;
+	public string axis = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//Update every interval, physics stuff.
+	void FixedUpdate(){
+
+		float v = Input.GetAxisRaw (axis);
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (0, v) * speed;
+	
+	}
+}
