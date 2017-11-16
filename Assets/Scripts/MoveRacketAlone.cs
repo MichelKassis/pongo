05e116a4c2309a36cbfb3f682adef282c214287e
Michel Kassis <michel.kassis120@gmail.com>
2017-11-15 21:49:55 -0500

    Make different scenes and menu page

---
+++ Assets/Scripts/MoveRacketAlone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacketAlone : MonoBehaviour
+{
+
+	float barPos;
+
+	// Use this for initialization
+	void Start()
+	{
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		barPos = GameObject.Find("Ball").transform.position.y;
+
+		Vector3 tmpPosition = transform.position;
+		tmpPosition.y = barPos;
+		transform.position = tmpPosition;
+
+	}
+}
